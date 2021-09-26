ac3eb60ba2c59528cbb1bda4c8d86ac9c8d5db43
JenilKundaliya <71178751+JenilKundaliya@users.noreply.github.com>
2021-09-26 11:00:18 +0530

    first commit git

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express();
 
-
+const cors = require("cors")
 const path = require("path")
 const PORT = process.env.PORT || 5000;
 require('./db/conn')
@@ -18,10 +18,9 @@ const data = require('./data/data.js')
 dotenv.config()
 const generateToken = require('./utils.js')
 // const isAuth = require('./utils.js')
-
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-
 
 app.get('/seed',async(req,res)=>{
     try{
